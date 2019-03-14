2e1bd2a392c8a354716544647561da0e0af0b3c7
Raymond <lebooray@gmail.com>
2019-03-14 13:21:08 -0400

    change

--- app_server/controllers/blog.js
+++ app_server/controllers/blog.js
@@ -67,7 +67,7 @@ var renderShowPage = function(req, res, responseBody){
 
 
 /* Book Add */
-module.exports.blogAdd = function(req, res) {
+module.exports.add = function(req, res) {
     res.render('blogAdd', { title: 'Blog Add' });
 };    
 
