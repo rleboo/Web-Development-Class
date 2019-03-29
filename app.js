b8b123f565e5d5636f5529663b80ea5ef6561657
Raymond <lebooray@gmail.com>
2019-03-29 19:29:02 -0400

    lol

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var routesApi = require('./app_api/routes/index');
 var app = express();
 
 // view engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
