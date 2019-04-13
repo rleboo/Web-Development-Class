5cea41a7df486458a2b29cc0f14d29b87af00c98
Raymond <lebooray@gmail.com>
2019-04-12 21:20:01 -0400

    changes

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,16 +1,22 @@
 var express = require('express');
 var router = express.Router();
-// var homeLocations = require('../controllers/home');
+var jwt = require('express-jwt'); 
+var auth = jwt({   // Lab 6
+  secret: process.env.JWT_SECRET,
+  userProperty: 'payload'
+});
+
+
 var blogLocations = require('../controllers/blog');
 var ctrlAuth = require('../controllers/authentication');
 
-router.post('/blogs/', blogLocations.blogCreate);
+router.post('/blogs/', auth, blogLocations.blogCreate);
 
 router.get('/blogs/:blogid', blogLocations.blogReadOne);
 router.get('/blogs/', blogLocations.blogList);
 
-router.put('/blogs/:blogid', blogLocations.blogUpdate);
+router.put('/blogs/:blogid', auth, blogLocations.blogUpdate);
 
-router.delete('/blogs/:blogid', blogLocations.blogDelete);
+router.delete('/blogs/:blogid', auth, blogLocations.blogDelete);
 
 module.exports = router;
