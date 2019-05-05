fff802ce7dd12d55491eeafef09ce43746ff59f6
Ubuntu <rleboo@github.com>
2019-05-05 16:15:08 +0000

    changes

--- app_api/models/blog.js
+++ app_api/models/blog.js
@@ -1,6 +1,12 @@
 
 var mongoose = require('mongoose');
 
+var commentSchema = new mongoose.Schema({
+	comment_text: {type: String, required: true},
+	email: {type: String, required: true},
+	name: {type: String, required: true}
+});
+
 var blogSchema = new mongoose.Schema({
 	blog_title: {type: String, required: true},
 	blog_text: {type: String, required: true},
@@ -9,7 +15,8 @@ var blogSchema = new mongoose.Schema({
 	createdOn: {
 	  type: String,
 	  "default": Date.now.toString()
-	}
+	},
+	comment: [commentSchema]
 });
 
 mongoose.model('Blog', blogSchema);
